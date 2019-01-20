42f5ff651329babb23c290375fea3b02814e3d18
sujoyDU <sujoydebnath.92@gmail.com>
2019-01-19 22:28:49 -0500

    compressed files

--- zipFile.py
+++ zipFile.py
@@ -5,11 +5,11 @@ from zipfile import ZipFile, ZipInfo, ZIP_DEFLATED, is_zipfile
 
 def helpInfo():
     print('''
-        This program compresses those files in a given folder that are bigger than a given size.\n
+        This program compresses those files in a given folder that are bigger than a given size and achive more than 25% compress ratio.\n
         This program takes two parameters.\n
             a. First parameter is a String that is Path of the Folder to compress.
             b. Second paramenter is an Integer that is MinimumSize in Bytes.\n
-        This program will compress those files whose file size is bigger than the MinimumSize.\n
+        This program will compress those files whose file size is bigger than the MinimumSize and attain compress ratio better than 25%.\n
         If No MinimumSize is given this will compress all the files in the folder.
     ''')
 
@@ -22,43 +22,39 @@ def getRatio(path):
 
         with ZipFile(delFile, 'r') as archive: 
             for info in archive.infolist(): 
-                    description = info.filename + '\n' \
-                                    + ('\tModified:\t' + str(datetime.datetime(*info.date_time))) + '\n' \
-                                    + ('\tSystem:\t\t' + str(info.create_system) + '(0 = Windows, 3 = Unix)') + '\n' \
-                                    + ('\tZIP version:\t' + str(info.create_version)) + '\n' \
-                                    + ('\tCompressed:\t' + str(info.compress_size) + ' bytes') + '\n' \
-                                    + ('\tUncompressed:\t' + str(info.file_size) + ' bytes') + '\n'               
-        print(description)
+                    ratio = (info.compress_size/info.file_size)*100
         
-    
-    return 5       
+    compressRatio = 100 - ratio
+    print(compressRatio)
+    return compressRatio
         
 
 
 
-def compressFile(path,size=0):
+def compressedFile(path,size=0):
     try:     
         description = 'Description of archive code\n'
-        if(os.path.exists(path)):
-            outputFileName = 'archive.zip'
-            outputZip = ZipFile(outputFileName, 'w')
-
+        outputFileName = 'archive.zip'
+        outputZip = ZipFile(outputFileName, 'w')
+        if(os.path.exists(path)):       
             for folder,subfolders, files in os.walk(path):
                 for fileName in files:
                     fsize = os.stat(os.path.join(folder,fileName)).st_size
                     if(fsize < size):
-                        description += f'{fileName} has file size of {fsize} which is less than minimum size {size}\n'
+                        description += f'{fileName} has file size of {fsize} Bytes which is less than minimum size {size} Bytes\n'
                     elif is_zipfile(os.path.join(folder,fileName)):
                         description += f'{fileName} is already an achived file\n'        
                     else:
-                        description +=f'{fileName} has file size of {fsize} which is greater than minimum size {size}\n'
+                        description +=f'{fileName} has file size of {fsize} Bytes which is greater than minimum size {size} Bytes\n'
                         ratio = getRatio(os.path.join(folder,fileName))
 
+                        if(ratio > 25):
+                            zippedFilePath = os.path.join(folder, fileName)
+                            zippedFileName = fileName
 
-                        zippedFilePath = os.path.join(folder, fileName)
-                        zippedFileName = fileName
-
-                        outputZip.write(zippedFilePath,zippedFileName, compress_type=ZIP_DEFLATED)                                                        
+                            outputZip.write(zippedFilePath,zippedFileName, compress_type=ZIP_DEFLATED)      
+                        else:
+                            description += f'{fileName} would achive {ratio:.2f}% compress ratio. Which is not beneficial\n'                                                  
         else:
             description += 'Path is invalid\n'
     
@@ -67,7 +63,7 @@ def compressFile(path,size=0):
         if(os.path.exists(outputFileName)):
             with ZipFile(outputFileName, 'r') as zip: 
                 for info in zip.infolist(): 
-                        description = info.filename + '\n' \
+                        description += info.filename + '\n' \
                                        + ('\tModified:\t' + str(datetime.datetime(*info.date_time))) + '\n' \
                                        + ('\tSystem:\t\t' + str(info.create_system) + '(0 = Windows, 3 = Unix)') + '\n' \
                                        + ('\tZIP version:\t' + str(info.create_version)) + '\n' \
@@ -78,47 +74,31 @@ def compressFile(path,size=0):
 
 
 
-# def send_email(recipient):
+def send_email(recipient):
 
-#     pattern = "^.+@(\[?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$"
-#     if re.match(pattern,recipient)!=None :
+    pattern = "^.+@(\[?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$"
+    if re.match(pattern,recipient)!=None :
 
-#         FROM = MY_ADDRESS
-#         TO = recipient
-#         SUBJECT = 'description of the code'
-#         TEXT = DESCRIPTION
+        FROM = MY_ADDRESS
+        TO = recipient
+        SUBJECT = 'description of the code'
+        TEXT = DESCRIPTION
 
-#         message = """From: %s\nTo: %s\nSubject: %s\n\n%s""" % (FROM, "".join(TO), SUBJECT, TEXT)
+        message = """From: %s\nTo: %s\nSubject: %s\n\n%s""" % (FROM, "".join(TO), SUBJECT, TEXT)
 
-#         # print(message)
-#         try:
-#             server = smtplib.SMTP(HOST,PORT)
-#             server.ehlo()
-#             server.starttls()
-#             server.login(MY_ADDRESS,PASSWORD)
-#             server.sendmail(FROM, TO, message)
-#             server.close()
-#             print('successfully sent the mail')
-#         except:
-#             print("failed to send mail")
-#     else:
-#         print('please enter correct email address')
+        # print(message)
+        try:
+            server = smtplib.SMTP(HOST,PORT)
+            server.ehlo()
+            server.starttls()
+            server.login(MY_ADDRESS,PASSWORD)
+            server.sendmail(FROM, TO, message)
+            server.close()
+            print('successfully sent the mail')
+        except:
+            print("failed to send mail")
+    else:
+        print('please enter correct email address')
 
 # helpInfo()
-# compressFile('C:\abcd', 1000000)
-
-import tempfile
-with tempfile.TemporaryFile() as delFile:
-    outputFile = ZipFile(delFile,'w')
-    outputFile.write('bfsdfs.py','b.py',compress_type=ZIP_DEFLATED)
-    outputFile.close()
-
-    with ZipFile(delFile, 'r') as zip: 
-        for info in zip.infolist(): 
-                description = info.filename + '\n' \
-                                + ('\tModified:\t' + str(datetime.datetime(*info.date_time))) + '\n' \
-                                + ('\tSystem:\t\t' + str(info.create_system) + '(0 = Windows, 3 = Unix)') + '\n' \
-                                + ('\tZIP version:\t' + str(info.create_version)) + '\n' \
-                                + ('\tCompressed:\t' + str(info.compress_size) + ' bytes') + '\n' \
-                                + ('\tUncompressed:\t' + str(info.file_size) + ' bytes') + '\n'               
-    print(description)       
+# compressedFile('C:\FolderToUse', 100000)
